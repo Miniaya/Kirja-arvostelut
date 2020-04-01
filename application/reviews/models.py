4e2ffc79cfa442e45f0442aa3d7d902f5ab4993b
Miniaya <henna.makiluoma@gmail.com>
2020-04-01 17:17:15 +0300

    Sovelluksen tyylittelyä

--- application/reviews/models.py
+++ application/reviews/models.py
@@ -1,9 +1,8 @@
 from application import db
+from application.models import Base
 
-class Review(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    date_created = db.Column(db.DateTime, default = db.func.current_timestamp())
-
+class Review(Base):
+    
     account_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable = False)
     book_id = db.Column(db.Integer, nullable = False)
     review = db.Column(db.String(144), nullable = False)
