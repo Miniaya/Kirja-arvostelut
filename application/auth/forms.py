9c784c53e0eef3bf4f6dfc6ae75b71e7c0303416
Miniaya <henna.makiluoma@gmail.com>
2020-04-30 18:12:54 +0300

    Validoinnin muokkaus ja requirements päivitys

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,6 +1,5 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, validators
-from wtforms_validators import AlphaSpace
 
 class LoginForm(FlaskForm):
     username = StringField("Username")
@@ -10,7 +9,7 @@ class LoginForm(FlaskForm):
         csrf = False
 
 class SignInForm(FlaskForm):
-    name = StringField("Full name", [validators.length(max=100), AlphaSpace()])
+    name = StringField("Full name", [validators.length(max=100)])
     username = StringField("Username", [validators.length(min=1, max=24)])
     password = PasswordField("Password", [validators.length(min=3), validators.length(max=16)])
 
--- application/read/forms.py
+++ application/read/forms.py
@@ -1,9 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, validators
-from wtforms_validators import AlphaSpace
 
 class ReadForm(FlaskForm):
-    author = StringField("Author", [validators.Length(min=3, max=144), AlphaSpace()])
+    author = StringField("Author", [validators.Length(min=3, max=144)])
     name = StringField("Book name", [validators.Length(min=2, max=144)])
     read = BooleanField("I've read this book")
 
